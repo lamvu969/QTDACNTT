f4382f6115149378fb896f49ded56a1f86677c3c
Qui07 <82872947+Qui07@users.noreply.github.com>
2021-06-28 08:22:16 +0700

    formDangnhap

---
+++ fDangNhap.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace DOAN.NET
+{
+    partial class fDangNhap
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(fDangNhap));
+            this.txt_hienthi = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_mk = new System.Windows.Forms.TextBox();
+            this.txt_tk = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.thoátToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txt_hienthi
+            // 
+            this.txt_hienthi.AutoSize = true;
+            this.txt_hienthi.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.txt_hienthi.Location = new System.Drawing.Point(147, 146);
+            this.txt_hienthi.Name = "txt_hienthi";
+            this.txt_hienthi.Size = new System.Drawing.Size(76, 21);
+            this.txt_hienthi.TabIndex = 16;
+            this.txt_hienthi.Text = "Hiển thị";
+            this.txt_hienthi.UseVisualStyleBackColor = true;
+            this.txt_hienthi.CheckedChanged += new System.EventHandler(this.txt_hienthi_CheckedChanged_1);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label3.ForeColor = System.Drawing.Color.Blue;
+            this.label3.Location = new System.Drawing.Point(223, 42);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(262, 45);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "ĐĂNG NHẬP";
+            // 
+            // txt_mk
+            // 
+            this.txt_mk.Location = new System.Drawing.Point(147, 108);
+            this.txt_mk.Name = "txt_mk";
+            this.txt_mk.Size = new System.Drawing.Size(192, 22);
+            this.txt_mk.TabIndex = 12;
+            // 
+            // txt_tk
+            // 
+            this.txt_tk.Location = new System.Drawing.Point(147, 56);
+            this.txt_tk.Name = "txt_tk";
+            this.txt_tk.Size = new System.Drawing.Size(192, 22);
+            this.txt_tk.TabIndex = 11;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label2.Location = new System.Drawing.Point(41, 107);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 22);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Mật khẩu:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label1.Location = new System.Drawing.Point(35, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 22);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Tài khoản:";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.LinkBehavior = System.Windows.Forms.LinkBehavior.HoverUnderline;
+            this.linkLabel1.LinkColor = System.Drawing.Color.Red;
+            this.linkLabel1.Location = new System.Drawing.Point(262, 173);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(117, 17);
+            this.linkLabel1.TabIndex = 17;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Quên mật khẩu ?";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.groupBox1.Controls.Add(this.txt_tk);
+            this.groupBox1.Controls.Add(this.linkLabel1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txt_hienthi);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txt_mk);
+            this.groupBox1.Location = new System.Drawing.Point(111, 106);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(479, 207);
+            this.groupBox1.TabIndex = 18;
+            this.groupBox1.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(12, 446);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(438, 19);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "* Lưu ý đăng nhập với quyền user sẽ không thể đổi mật khẩu";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Visible;
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.thoátToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(729, 28);
+            this.menuStrip1.TabIndex = 70;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // thoátToolStripMenuItem
+            // 
+            this.thoátToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("thoátToolStripMenuItem.Image")));
+            this.thoátToolStripMenuItem.Name = "thoátToolStripMenuItem";
+            this.thoátToolStripMenuItem.Size = new System.Drawing.Size(79, 24);
+            this.thoátToolStripMenuItem.Text = "Thoát";
+            this.thoátToolStripMenuItem.Click += new System.EventHandler(this.thoátToolStripMenuItem_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button3.BackgroundImage")));
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.button3.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.ImageAlign = System.Drawing.ContentAlignment.BottomLeft;
+            this.button3.Location = new System.Drawing.Point(171, 14);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(138, 47);
+            this.button3.TabIndex = 71;
+            this.button3.Text = "Đăng nhập";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Location = new System.Drawing.Point(111, 317);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(479, 77);
+            this.groupBox2.TabIndex = 19;
+            this.groupBox2.TabStop = false;
+            // 
+            // fDangNhap
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(194)))), ((int)(((byte)(217)))), ((int)(((byte)(247)))));
+            this.ClientSize = new System.Drawing.Size(729, 474);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "fDangNhap";
+            this.Text = "Đăng Nhập Phần Mềm Quản Lý Đoàn Viên";
+            this.Load += new System.EventHandler(this.fDangNhap_Load_1);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox txt_hienthi;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_mk;
+        private System.Windows.Forms.TextBox txt_tk;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem thoátToolStripMenuItem;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
+
---
+++ fDangNhap.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace DOAN.NET
+{
+    public partial class fDangNhap : Form
+    {
+        public fDangNhap()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            DialogResult tb = MessageBox.Show("Bạn có muốn thoát không?", "Thông báo", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+            if (tb == DialogResult.OK)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void txt_hienthi_CheckedChanged_1(object sender, EventArgs e)
+        {
+            if (txt_hienthi.Checked)
+            {
+                txt_mk.UseSystemPasswordChar = false;
+            }
+            else
+            {
+                txt_mk.UseSystemPasswordChar = true;
+            }
+        }
+
+        private void fDangNhap_Load_1(object sender, EventArgs e)
+        {
+            txt_mk.UseSystemPasswordChar = true;
+        }
+
+        private void thoátToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DialogResult tb = MessageBox.Show("Bạn có muốn thoát không?", "Thông báo", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+            if (tb == DialogResult.OK)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(@"Data Source=DESKTOP-EEAM3B1\SQLEXPRESS;Initial Catalog=QLDV_DoAn.NET;Integrated Security=True");
+            try
+            {
+                conn.Open();
+                string tk = txt_tk.Text;
+                string mk = txt_mk.Text;
+                string sql = "select *from NguoiDung where taikhoan ='" + tk + "' and matkhau = '" + mk + "' and quyen='Admin' ";
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                SqlDataReader dta = cmd.ExecuteReader();
+
+                if (dta.Read())
+                {
+                    MessageBox.Show("Đăng nhập thành công (Quyền Admin)", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    TrangChu.quyen = "Admin";
+                    TrangChu frm = new TrangChu();
+
+                    frm.Show();
+                    string k = txt_tk.Text;
+                    frm.Text = "Chào Mừng " + txt_tk.Text + " Đã Quay Trở Lại Với Chương Trình Quản Lý Đoàn Viên Trường Đại Học Điện Lực";
+                    this.Hide();
+                    cmd.Dispose();
+                    dta.Close();
+                    dta.Dispose();
+                }
+                else
+                {
+                    cmd.Dispose();
+                    dta.Close();
+                    dta.Dispose();
+                    string sql1 = "select *from NguoiDung where taikhoan ='" + tk + "' and matkhau = '" + mk + "' and quyen='user' ";
+                    SqlCommand cmd1 = new SqlCommand(sql1, conn);
+                    SqlDataReader dta1;
+                    dta1 = cmd1.ExecuteReader();
+
+                    if (dta1.Read())
+                    {
+                        MessageBox.Show("Đăng nhập thành công (Quyền user)", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        TrangChu.quyen = "User";
+                        TrangChu frm = new TrangChu();
+
+                        frm.Show();
+                        string k = txt_tk.Text;
+                        frm.Text = "Chào Mừng " + txt_tk.Text + " Đã Quay Trở Lại Với Chương Trình Quản Lý Đoàn Viên Trường Đại Học Điện Lực";
+                        this.Hide();
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("Tài khoản hoặc mật khẩu không chính xác", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    cmd1.Dispose();
+                    dta1.Close();
+                    dta1.Dispose();
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Lỗi kết nối!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
