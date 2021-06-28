7e99bc61e5416a786746dac160d2b5d96439fe14
Hoang992000 <82872560+Hoang992000@users.noreply.github.com>
2021-06-28 08:53:22 +0700

    Add files via upload

---
+++ SBT.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace DOAN.NET
+{
+    class SBT
+    {
+        public const string con_str = @"Data Source=DESKTOP-EEAM3B1\SQLEXPRESS;Initial Catalog=QLDV_DoAn.NET;Integrated Security=True";
+        public static SqlConnection conn = new SqlConnection(con_str);
+        
+    }
+}
---
+++ SBt2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DOAN.NET
+{
+    class SBt2
+    {
+        private SqlConnection conn;
+        // Trả về đối tượng kết nối
+        public SqlConnection Connected()
+        {
+            string conect = SystemInformation.UserDomainName.ToString();
+
+            string source = @"Data Source=DESKTOP-EEAM3B1\SQLEXPRESS;Initial Catalog=QLDV_DoAn.NET;Integrated Security=True";
+            conn = new SqlConnection(source);
+            conn.Open();
+            return conn;
+        }
+    }
+}
