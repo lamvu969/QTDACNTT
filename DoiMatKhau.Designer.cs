c7118cb5da7b3f7629b0f1dd8425f4174988f423
Hoang992000 <82872560+Hoang992000@users.noreply.github.com>
2021-06-28 08:26:30 +0700

    Add files via upload

---
+++ DoiMatKhau.Designer.cs
@@ -0,0 +1,301 @@
+﻿namespace DOAN.NET
+{
+    partial class DoiMatKhau
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DoiMatKhau));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.quayLạiToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtgolai = new System.Windows.Forms.TextBox();
+            this.txtmkmoi = new System.Windows.Forms.TextBox();
+            this.txtmkcu = new System.Windows.Forms.TextBox();
+            this.txttk = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnnhaplai = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.btndoimk = new System.Windows.Forms.Button();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.quayLạiToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 89;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // quayLạiToolStripMenuItem
+            // 
+            this.quayLạiToolStripMenuItem.Image = global::DOAN.NET.Properties.Resources.Custom_Icon_Design_Flatastic_8_Go_back;
+            this.quayLạiToolStripMenuItem.Name = "quayLạiToolStripMenuItem";
+            this.quayLạiToolStripMenuItem.Size = new System.Drawing.Size(95, 24);
+            this.quayLạiToolStripMenuItem.Text = "Quay lại";
+            this.quayLạiToolStripMenuItem.Click += new System.EventHandler(this.quayLạiToolStripMenuItem_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.groupBox1.Controls.Add(this.txtgolai);
+            this.groupBox1.Controls.Add(this.txtmkmoi);
+            this.groupBox1.Controls.Add(this.txtmkcu);
+            this.groupBox1.Controls.Add(this.txttk);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.ForeColor = System.Drawing.Color.White;
+            this.groupBox1.Location = new System.Drawing.Point(147, 115);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(493, 211);
+            this.groupBox1.TabIndex = 96;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Đổi mật khẩu";
+            // 
+            // txtgolai
+            // 
+            this.txtgolai.Location = new System.Drawing.Point(187, 154);
+            this.txtgolai.Margin = new System.Windows.Forms.Padding(4);
+            this.txtgolai.Name = "txtgolai";
+            this.txtgolai.PasswordChar = '*';
+            this.txtgolai.Size = new System.Drawing.Size(184, 26);
+            this.txtgolai.TabIndex = 1;
+            // 
+            // txtmkmoi
+            // 
+            this.txtmkmoi.Location = new System.Drawing.Point(187, 114);
+            this.txtmkmoi.Margin = new System.Windows.Forms.Padding(4);
+            this.txtmkmoi.Name = "txtmkmoi";
+            this.txtmkmoi.PasswordChar = '*';
+            this.txtmkmoi.Size = new System.Drawing.Size(184, 26);
+            this.txtmkmoi.TabIndex = 1;
+            // 
+            // txtmkcu
+            // 
+            this.txtmkcu.Location = new System.Drawing.Point(187, 76);
+            this.txtmkcu.Margin = new System.Windows.Forms.Padding(4);
+            this.txtmkcu.Name = "txtmkcu";
+            this.txtmkcu.PasswordChar = '*';
+            this.txtmkcu.Size = new System.Drawing.Size(184, 26);
+            this.txtmkcu.TabIndex = 1;
+            // 
+            // txttk
+            // 
+            this.txttk.Location = new System.Drawing.Point(187, 38);
+            this.txttk.Margin = new System.Windows.Forms.Padding(4);
+            this.txttk.Name = "txttk";
+            this.txttk.Size = new System.Drawing.Size(184, 26);
+            this.txttk.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(37, 158);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(115, 19);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Gõ lại mật khẩu";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(51, 118);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(102, 19);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Mật khẩu mới";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(56, 80);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(91, 19);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Mật khẩu cũ";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(44, 41);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(103, 19);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "Tên Tài khoản";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.SystemColors.Highlight;
+            this.groupBox2.Controls.Add(this.btnnhaplai);
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Controls.Add(this.btndoimk);
+            this.groupBox2.Location = new System.Drawing.Point(147, 328);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Size = new System.Drawing.Size(493, 79);
+            this.groupBox2.TabIndex = 97;
+            this.groupBox2.TabStop = false;
+            // 
+            // btnnhaplai
+            // 
+            this.btnnhaplai.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnnhaplai.BackgroundImage")));
+            this.btnnhaplai.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnnhaplai.FlatAppearance.BorderSize = 0;
+            this.btnnhaplai.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btnnhaplai.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.btnnhaplai.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnnhaplai.ForeColor = System.Drawing.Color.Black;
+            this.btnnhaplai.ImageAlign = System.Drawing.ContentAlignment.BottomLeft;
+            this.btnnhaplai.Location = new System.Drawing.Point(210, 23);
+            this.btnnhaplai.Margin = new System.Windows.Forms.Padding(4);
+            this.btnnhaplai.Name = "btnnhaplai";
+            this.btnnhaplai.Size = new System.Drawing.Size(108, 36);
+            this.btnnhaplai.TabIndex = 1;
+            this.btnnhaplai.Text = "Nhập lại";
+            this.btnnhaplai.UseVisualStyleBackColor = true;
+            this.btnnhaplai.Click += new System.EventHandler(this.btnnhaplai_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button3.BackgroundImage")));
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.button3.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.ImageAlign = System.Drawing.ContentAlignment.BottomLeft;
+            this.button3.Location = new System.Drawing.Point(349, 23);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(108, 36);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "Thoát";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // btndoimk
+            // 
+            this.btndoimk.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btndoimk.BackgroundImage")));
+            this.btndoimk.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btndoimk.FlatAppearance.BorderSize = 0;
+            this.btndoimk.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btndoimk.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.btndoimk.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btndoimk.ForeColor = System.Drawing.Color.Black;
+            this.btndoimk.ImageAlign = System.Drawing.ContentAlignment.BottomLeft;
+            this.btndoimk.Location = new System.Drawing.Point(72, 23);
+            this.btndoimk.Margin = new System.Windows.Forms.Padding(4);
+            this.btndoimk.Name = "btndoimk";
+            this.btndoimk.Size = new System.Drawing.Size(108, 36);
+            this.btndoimk.TabIndex = 0;
+            this.btndoimk.Text = "Đổi mât khẩu";
+            this.btndoimk.UseVisualStyleBackColor = true;
+            this.btndoimk.Click += new System.EventHandler(this.btndoimk_Click);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(251, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(312, 43);
+            this.label1.TabIndex = 98;
+            this.label1.Text = "ĐỔI MẬT KHẨU";
+            // 
+            // DoiMatKhau
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "DoiMatKhau";
+            this.Text = "Đổi mật khẩu";
+            this.Load += new System.EventHandler(this.TaiKhoan_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem quayLạiToolStripMenuItem;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtgolai;
+        private System.Windows.Forms.TextBox txtmkmoi;
+        private System.Windows.Forms.TextBox txtmkcu;
+        private System.Windows.Forms.TextBox txttk;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnnhaplai;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btndoimk;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ DoiMatKhau.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace DOAN.NET
+{
+    public partial class DoiMatKhau : Form
+    {
+        private SBt2 cc = new SBt2();
+        SqlConnection conn = null;
+        public DoiMatKhau()
+        {
+            InitializeComponent();
+        }
+
+        private void quayLạiToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            TrangChu frm = new TrangChu();
+            frm.Show();
+            this.Hide();
+        }
+
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DialogResult tb = MessageBox.Show("Bạn có muốn thoát không?", "Thông báo", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+            if (tb == DialogResult.OK)
+            {
+                Application.Exit();
+            }
+        }
+
+
+        private void btndoimk_Click(object sender, EventArgs e)
+        {
+            string select2 = "Select * From NguoiDung where taikhoan='" + txttk.Text + "' and matkhau='" + txtmkcu.Text + "' ";
+            SqlCommand cmd2 = new SqlCommand(select2, conn);
+            SqlDataReader reader2;
+            reader2 = cmd2.ExecuteReader();
+
+
+
+
+            errorProvider1.Clear();
+            if (txttk.Text == "")
+                errorProvider1.SetError(txttk, "Chưa nhập tên tài khoản !");
+            else if (txtmkcu.Text == "")
+            {
+                errorProvider1.SetError(txtmkcu, "!");
+            }
+            else if (txtmkmoi.Text == "")
+            {
+                errorProvider1.SetError(txtmkmoi, "!");
+                //txtmkmoi.Focus();
+            }
+            else if (txtgolai.Text == "")
+            {
+                errorProvider1.SetError(txtgolai, "!");
+                //txtgolai.Focus();
+            }
+            
+            else if (txtgolai.Text != txtmkmoi.Text)
+                MessageBox.Show("Bạn nhập lại password không trùng khớp", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            else if (reader2.Read())
+            {
+                cmd2.Dispose();
+                reader2.Dispose();
+                // Thực hiện truy vấn
+                string update = "Update NguoiDung Set matkhau='" + txtmkmoi.Text + "' where taikhoan='" + txttk.Text + "'";
+                SqlCommand cmd = new SqlCommand(update, conn);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Đổi mật khẩu thành công", "Thông báo!");
+
+                // Trả tài nguyên
+                cmd.Dispose();
+                ClearText();
+            }
+
+            else
+            {
+                MessageBox.Show("Tên tài khoản không tồn tại hoặc mật khẩu sai! ", "Thông báo !", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txttk.Focus();
+
+            }
+            cmd2.Dispose();
+            reader2.Dispose();
+        }
+        private void ClearText()
+        {
+            txttk.Clear();
+            txtmkcu.Clear();
+            txtmkmoi.Clear();
+            txtgolai.Clear();
+        }
+            private void btnnhaplai_Click(object sender, EventArgs e)
+        {
+            ClearText();
+        }
+
+        private void TaiKhoan_Load(object sender, EventArgs e)
+        {
+            conn = cc.Connected();
+        }
+    }
+}
